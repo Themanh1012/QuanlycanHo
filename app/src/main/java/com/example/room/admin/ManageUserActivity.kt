46de51bcffd774af4d64dcfe3c04eac5befd519a
TheManh <manh31312@gmail.com>
2026-03-22 20:57:13 +0700

    sửa apartment

--- app/src/main/java/com/example/room/admin/ManageUserActivity.kt
+++ app/src/main/java/com/example/room/admin/ManageUserActivity.kt
@@ -1,21 +1,45 @@
 package com.example.room.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.room.LoginActivity
 import com.example.room.R
+import com.example.room.AddPostActivity // Đổi sang AddPostActivity theo file của team bạn
 
 class ManageUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_manage_user)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val btnAddApartment = findViewById<Button>(R.id.btnAddApartment)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+
+        btnAddApartment.setOnClickListener {
+
+            val intent = Intent(this, AddPostActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
